26732c31e23b51604d236119bb0c7cf8bb88b32a
こが <kogarasi.cross@gmail.com>
2018-02-18 12:32:14 +0900

    InputFieldにColorTintElementを対応

--- Scripts/UI/InputField.cs
+++ Scripts/UI/InputField.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using UnityEngine.Events;
 
 using uGUIs.Attribute;
+using uGUIs.Style.Element;
 
 namespace uGUIs.UI {
   public class InputField : UI<InputField, UnityEngine.UI.InputField>, Interface.Callback {
@@ -26,6 +27,18 @@ namespace uGUIs.UI {
       if(onValueChangedCallback != null){
         ui.onValueChanged.RemoveListener(onValueChangedCallback);
       }
+    }
+
+    [Connect(typeof(ColorTintElement))]
+    void applyColorTintFont(ColorTintElement elem){
+      ui.transition = Selectable.Transition.ColorTint;
+
+      var colors = ui.colors;
+      colors.normalColor = elem.normal;
+      colors.highlightedColor = elem.highlighted;
+      colors.pressedColor = elem.pressed;
+      colors.disabledColor = elem.disabled;
+      ui.colors = colors;
     }
   }
 }
